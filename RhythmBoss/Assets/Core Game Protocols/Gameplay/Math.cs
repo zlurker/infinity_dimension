79decc967184e51a5760886c91e9023f8cd2bda5
zlurker <tallspin_97@hotmail.com>
2017-11-05 01:35:19 +0800

    Angle calculator completed
    Completed a angle calculator which returns magnituide and direction on
    Math.cs CalculateAngle().

---
+++ RhythmBoss/Assets/Core Game Protocols/Gameplay/Math.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Math {
+
+    public static float CalculateAngle(Vector2[] pTC) {
+        Vector2 d = pTC[1] - pTC[0];
+        int m = 0;
+        int u = 45;
+
+        for (int i = 0; i < 2; i++)
+            if (pTC[0][i] == 0 && pTC[1][i] == 0)
+                m += 2;
+
+        u = (pTC[0].y * d.x) + (pTC[0].x * d.y * -1) >= 0 ? u : u * -1;
+
+        return (Mathf.Abs(d.x) + Mathf.Abs(d.y) + m) * u;
+    }
+
+    public static Vector2 Normalise(Vector2 tTN) {
+        return tTN;
+    }
+}
---
+++ RhythmBoss/Assets/Core Game Protocols/Gameplay/Projectile/Projectile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public Vector2[] pTC;
+
+    void Start() {
+
+        Debug.Log(Math.CalculateAngle(pTC));
+    }
+
+    
+}
