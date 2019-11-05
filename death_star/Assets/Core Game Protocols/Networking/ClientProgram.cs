3ea24ae2d185357e95e98fbd2ae7b8e8189fa0e3
zlurker <tallspin_97@hotmail.com>
2019-11-05 21:06:49 +0800

    Basic networking test
    Success, however small bug whereby if server is closed, unity will be unable to connect to that port/ip address unless we end the task. Might be something to do with thread.

--- death_star/Assets/Core Game Protocols/Networking/ClientProgram.cs
+++ death_star/Assets/Core Game Protocols/Networking/ClientProgram.cs
@@ -1,13 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Net.Sockets;
+using System.Text;
 
 public class ClientProgram : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private Socket clientSock;
+    private byte[] _recieveBuffer = new byte[8142];
+    // Use this for initialization
+    void Start () {
+        clientSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        clientSock.Connect("178.128.95.63", 5000);
+        clientSock.BeginReceive(_recieveBuffer,0,_recieveBuffer.Length,SocketFlags.None,new AsyncCallback(OnRecieve),null);
+
+    }
+
+    void OnRecieve(IAsyncResult asyncResult) {
+        int recieved = clientSock.EndReceive(asyncResult);
+
+        //Copy the recieved data into new buffer , to avoid null bytes
+        byte[] recData = new byte[recieved];
+        Buffer.BlockCopy(_recieveBuffer, 0, recData, 0, recieved);
+
+        //Process data here the way you want , all your bytes will be stored in recData
+        Debug.Log(Encoding.Default.GetString(recData, 0, recData.Length));
+
+        //Start receiving again
+        clientSock.BeginReceive(_recieveBuffer, 0, _recieveBuffer.Length, SocketFlags.None, new AsyncCallback(OnRecieve), null);
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
