3d0d57edf77e79b02d56d789363e11b326431183
zlurker <tallspin_97@hotmail.com>
2020-04-13 21:13:28 +0800

    Timer Class

---
+++ death_star/Assets/Core Game Protocols/Singletons/Timer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ITimerCallback {
+    void CallOnTimerEnd();
+}
+
+public class TimerData {
+
+    public float startTime;
+    public float duration;
+    public ITimerCallback callback;
+
+    public TimerData(float sT, float d, ITimerCallback cb) {
+        startTime = sT;
+        duration = d;
+        callback = cb;
+    }
+}
+
+public class Timer : MonoBehaviour, ISingleton {
+
+    EnhancedList<TimerData> tData;
+
+    void Update() {
+
+        for(int i = tData.l.Count; i >= 0; i--) {
+            if(tData.l[i] != null)
+                if(tData.l[i].startTime + tData.l[i].duration <= Time.realtimeSinceStartup) {
+                    tData.l[i].callback.CallOnTimerEnd();
+                    tData.Remove(i);
+                }
+        }
+    }
+
+    public int CreateNewTimerEvent(float d, ITimerCallback cb) {
+        return tData.Add(new TimerData(Time.realtimeSinceStartup, d, cb));
+    }
+
+    public int CreateNewTimerEvent(float sT, float d, ITimerCallback cb) {
+        return tData.Add(new TimerData(sT, d, cb));
+    }
+
+    public void UpdateEventStartTime(int eventId, float sT) {
+        tData.l[eventId].startTime = sT;
+    }
+
+    public void UpdateEventDuration(int eventId, float d) {
+        tData.l[eventId].duration = d;
+    }
+
+    public void RunOnCreated() {
+        tData = new EnhancedList<TimerData>();
+    }
+
+    public void RunOnStart() {
+    }
+}
