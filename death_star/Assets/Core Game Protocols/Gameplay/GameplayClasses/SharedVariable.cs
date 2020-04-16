c2d5930a5a834e68ff640ce16d13832a09292fa5
zlurker <tallspin_97@hotmail.com>
2020-04-16 11:46:53 +0800

    Shared Variable Implemented
    A node which keeps tracked of all same named variables in context and updates them when neccesary.

--- death_star/Assets/Core Game Protocols/Gameplay/GameplayClasses/SharedVariable.cs
+++ death_star/Assets/Core Game Protocols/Gameplay/GameplayClasses/SharedVariable.cs
@@ -2,39 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VariableInterfaces {
+public class VariableInterfaces: IRPGeneric {
+
+    public class InternalValueHolder<T> : InternalValueHolder {
+        public T value;
+    }
+
+    public class InternalValueHolder {
+
+    }
 
     public List<SharedVariable> sVList;
+    public InternalValueHolder valueHolder;
+    
+
+    public VariableInterfaces() {
+        sVList = new List<SharedVariable>();
+    }
+
+    public void Callback(SharedVariable caller) {
+        AbilityCentralThreadPool inst = caller.GetCentralInst();
+        inst.ReturnVariable(caller.GetNodeId(), caller.GetVariableId("Variable Value")).field.RunGenericBasedOnRP<SharedVariable>(this,caller);
+    }
+
+    public void RunAccordingToGeneric<T, P>(P arg) {
+        AbilityTreeNode node = arg as AbilityTreeNode;
+
+        AbilityCentralThreadPool inst = node.GetCentralInst();
+        T value = inst.ReturnRuntimeParameter<T>(node.GetNodeId(), node.GetVariableId("Variable Value")).v;
+
+        for(int i = 0; i < sVList.Count; i++)
+            sVList[i].SetVariable<T>("Variable Value", value);
+
+        InternalValueHolder<T> vhInst = valueHolder as InternalValueHolder<T>;
+
+        if(vhInst == null)
+            vhInst = new InternalValueHolder<T>();
+
+        vhInst.value = value;
+    }
 }
 
 public class SharedVariable : AbilityTreeNode, IOnSpawn {
 
-    public static Dictionary<int, VariableInterfaces> sharedVariables = new Dictionary<int, VariableInterfaces>();
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public static Dictionary<int, Dictionary<string,VariableInterfaces>> sharedVariables = new Dictionary<int, Dictionary<string, VariableInterfaces>>();
 
     public override void NodeCallback(int threadId) {
         base.NodeCallback(threadId);
-                
+
+        if (CheckIfVarRegionBlocked(GetVariableId("Variable Value"))) 
+            for (int i =0; i < GetVariableInterface().sVList.Count; i++) 
+                GetVariableInterface().Callback(this);        
     }
 
     public override void GetRuntimeParameters(List<LoadedRuntimeParameters> holder) {
         base.GetRuntimeParameters(holder);
 
         holder.AddRange(new LoadedRuntimeParameters[] {
-            new LoadedRuntimeParameters(new RuntimeParameters<string>("Variable Name","")),
+            new LoadedRuntimeParameters(new RuntimeParameters<string>("Variable Name",""),VariableTypes.AUTO_MANAGED),
             new LoadedRuntimeParameters(new RuntimeParameters<int>("Variable Value",0),VariableTypes.INTERCHANGEABLE)
         });
     }
 
     public void OnSpawn() {
-        
+        GetVariableInterface().sVList.Add(this);
+    }
+
+    VariableInterfaces GetVariableInterface() {
+
+        int clusterRootId = AbilityCentralThreadPool.globalCentralClusterList.l[GetCentralInst().GetClusterID()][0];
+
+        if(!sharedVariables.ContainsKey(clusterRootId))
+            sharedVariables.Add(clusterRootId, new Dictionary<string, VariableInterfaces>());
+
+        if(!sharedVariables[clusterRootId].ContainsKey(GetNodeVariable<string>("Variable Name")))
+            sharedVariables[clusterRootId].Add(GetNodeVariable<string>("Variable Name"), new VariableInterfaces());
+
+        return sharedVariables[clusterRootId][GetNodeVariable<string>("Variable Name")];
     }
 }
