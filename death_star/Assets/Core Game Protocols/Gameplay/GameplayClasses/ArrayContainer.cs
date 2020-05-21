c7428d2817db8db15dc5a734d516151ff178e27d
zlurker <tallspin_97@hotmail.com>
2020-05-21 14:54:18 +0800

    Random, Array System

---
+++ death_star/Assets/Core Game Protocols/Gameplay/GameplayClasses/ArrayContainer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayContainer : AbilityTreeNode, IRPGeneric {
+
+    public override void GetRuntimeParameters(List<LoadedRuntimeParameters> holder) {
+        base.GetRuntimeParameters(holder);
+
+        holder.AddRange(new LoadedRuntimeParameters[] {
+            new LoadedRuntimeParameters(new RuntimeParameters<int>("Values", 0),VariableTypes.PERMENANT_TYPE, VariableTypes.SIGNAL_ONLY, VariableTypes.NON_LINK),
+            new LoadedRuntimeParameters(new RuntimeParameters<int>("Array Element",0)),
+            new LoadedRuntimeParameters(new RuntimeParameters<int>("Output from Array",0), VariableTypes.INTERCHANGEABLE)
+        });
+    }
+
+    public override void NodeCallback() {
+        base.NodeCallback();
+
+        int[] arrayValues = GetCentralInst().ReturnVariable(GetNodeId(), GetVariableId("Values")).links[GetNodeVariable<int>("Array Element")];
+        RuntimeParameters targetVar = GetCentralInst().ReturnVariable(arrayValues[0], arrayValues[1]).field;
+        targetVar.RunGenericBasedOnRP<RuntimeParameters>(this, targetVar);
+    }
+
+    public void RunAccordingToGeneric<T, P>(P arg) {
+
+        RuntimeParameters<T> targetVar = (RuntimeParameters<T>)(object)arg;
+        SetVariable<T>("Output from Array", targetVar.v);
+    }
+}
---
+++ death_star/Assets/Core Game Protocols/Gameplay/GameplayClasses/RandomWholeNumber.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomWholeNumber : AbilityTreeNode {
+
+    public override void GetRuntimeParameters(List<LoadedRuntimeParameters> holder) {
+        base.GetRuntimeParameters(holder);
+
+        holder.AddRange(new LoadedRuntimeParameters[] {
+            new LoadedRuntimeParameters(new RuntimeParameters<int>("Min Value",0),VariableTypes.AUTO_MANAGED),
+            new LoadedRuntimeParameters(new RuntimeParameters<int>("Max Value",0),VariableTypes.AUTO_MANAGED),
+            new LoadedRuntimeParameters(new RuntimeParameters<int>("Output Value",0),VariableTypes.NETWORK)
+        });
+    }
+
+    public override void NodeCallback() {
+        base.NodeCallback();
+
+        if(IsHost()) {
+            //Debug.Log("Random called.");
+            int output = Random.Range(GetNodeVariable<int>("Min Value"), GetNodeVariable<int>("Max Value"));
+            Debug.Log("Random Int :" + output);
+            SetVariable<int>("Output Value", output);
+        }
+    }
+}
